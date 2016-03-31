852951f6267f69ed121ad2c0ca7dcd84786a79e0
cataclyzh <cataclyzh@163.com>
2016-03-31 14:15:47 +0800

    test verify code

---
+++ njhw/test/TestVerifyCode.java
@@ -0,0 +1,11 @@
+
+public class TestVerifyCode {
+
+	public static void main(String[] args) {
+		//String verificationCode = String.valueOf((int) ((Math.random() * 9 + 1) * 100000));
+		
+//		System.out.println(verificationCode);
+		System.out.println((Math.random() * 9) * 100000);
+	}
+
+}
